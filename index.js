222d16f469a06024bceb71a33c9fa5942005e88b
Aditya Mane <adityamane711@gmail.com>
2023-12-19 00:18:57 +0530

    Add payment

--- index.js
+++ index.js
@@ -3,62 +3,50 @@ import mysql from "mysql";
 import cors from "cors";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import subscribeRoutes from "./routes/subscribe.js";
+
 dotenv.config();
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-app.get("/", (req, res) => {
-  return res.json("From backend side");
-});
-
+// app.use("/subscribe", subscribeRoutes);
 const db = mysql.createConnection({
   host: "localhost",
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: "flexmoney",
 });
-
-app.get("/users", (req, res) => {
-  const q = "Select * from users";
-  db.query(q, (err, data) => {
-    if (err) return res.json(err);
-    return res.json(data);
-  });
-});
-app.get("/batch", (req, res) => {
-  const q = "Select * from batch";
-  db.query(q, (err, data) => {
-    if (err) return res.json(err);
-    return res.json(data);
-  });
-});
-app.post("/subscribe", (req, res) => {
-  const { name, email, age, batch_id } = req.body;
-
-  const q = "Insert into users (name,email,age,batch_id) VALUES (?,?,?,?)";
-
-  db.query(q, [name, email, age, batch_id], (err, data) => {
-    if (err) {
-      return res.json(err);
-    }
-
-    return res.status(200).json({ success: "Subscribed" });
-  });
-});
-app.post("/payment", (req, res) => {
-  const { user_id } = req.body;
-
+const paymentProcess = async (userId) => {
   const q = "Insert into payment (user_id,date) VALUES (?,NOW())";
-  db.query(q, user_id, (err, data) => {
+  db.query(q, userId, (err, data) => {
     if (err) {
       return res.json(err);
     }
     console.log("payment done");
-    return res.status(200).json({ success: "Payment successful" });
+    return;
   });
   return;
+};
+app.post("/subscribe", (req, res) => {
+  const { name, email, age, batchId } = req.body;
+  const q = "Insert into users (name,email,age,batch_id) VALUES (?,?,?,?)";
+  db.query(q, [name, email, age, batchId], async (err, data) => {
+    if (err) {
+      return res.json(err);
+    }
+    await paymentProcess(data.insertId);
+    return res.status(200).json({ success: "Subscribed" });
+  });
 });
+app.get("/batch", (err, res) => {
+  const q = "Select * from batch";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.status(200).json(data);
+  });
+});
+
 app.listen(3001, () => {
   console.log("listening");
 });
